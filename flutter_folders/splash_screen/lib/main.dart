430dc90189f4a7af88f2e3dca93ccbf79bcc9a8b
dawon <dawon337@gmail.com>
2023-12-24 23:45:21 +0900

    feat : splash_screen end

--- flutter_folders/splash_screen/lib/main.dart
+++ flutter_folders/splash_screen/lib/main.dart
@@ -9,8 +9,31 @@ class SplashScreen extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        body:Center(
-          child: Text('Splash Screen'),
+        body:Container(
+          decoration: BoxDecoration(
+            color: Color(0xFFF99231),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:[
+                  Image.asset(
+                    'assets/logo.png',
+                    width: 200,
+                  ),
+                  CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+
+
+            ],
+          ),
         ),
       ),
     );
