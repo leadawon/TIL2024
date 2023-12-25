de0d8092ad18c62899aafb4852b45d3f7930f5c3
dawon <dawon337@gmail.com>
2023-12-26 03:17:30 +0900

    feat : image_carousel end

--- flutter_folders/image_carousel/lib/screen/home_screen.dart
+++ flutter_folders/image_carousel/lib/screen/home_screen.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    Timer.periodic(
+      Duration(seconds: 3),
+          (timer) {
+        int? nextPage = pageController.page?.toInt();
+
+        if(nextPage==null){
+          return;
+        }
+
+        if(nextPage==4){
+          nextPage = 0;
+        }else{
+          nextPage++;
+        }
+
+        pageController.animateToPage(
+          nextPage,
+          duration:Duration(milliseconds: 500),
+          curve:Curves.ease,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView(
+        controller: pageController,
         children: [1, 2, 3, 4, 5]
             .map(
               (number) => Image.asset(
