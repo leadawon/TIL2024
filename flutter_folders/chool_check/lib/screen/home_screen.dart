205ef0b818fefbb6f6bbb1ed5282c3c3d79a2a7b
dawon <dawon337@gmail.com>
2024-01-08 22:48:14 +0900

    feat: google map api permissions

---
+++ flutter_folders/chool_check/lib/screen/home_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class HomeScreen extends StatelessWidget {
+  static final LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  static final Marker marker = Marker(
+    markerId: MarkerId('company'),
+    position: companyLatLng,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: FutureBuilder<String>(
+          future: checkPermission(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData &&
+                snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (snapshot.data == '위치 권한이 허가 되었습니다.') {
+              return Column(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: GoogleMap(
+                      initialCameraPosition: CameraPosition(
+                        target: companyLatLng,
+                        zoom: 16,
+                      ),
+                      markers:Set.from([marker])
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.timelapse_outlined,
+                            color: Colors.blue, size: 50.0),
+                        const SizedBox(height: 20.0),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Text('출근하기!'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            }
+            return Center(
+              child: Text(
+                snapshot.data.toString(),
+              ),
+            );
+          }),
+    );
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Text(
+        '오늘도 출첵',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled){
+      return '위치 서비스를 활성화해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied){
+      checkedPermission = await Geolocator.requestPermission();
+
+      if(checkedPermission == LocationPermission.denied){
+
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if(checkedPermission == LocationPermission.deniedForever){
+      return '앱의 위치 권한을 설정에서 허가해주세요.';
+    }
+
+    return '위치 권한이 허가 되었습니다.';
+
+
+  }
+}
--- flutter_folders/video_call/lib/screen/cam_screen.dart
+++ flutter_folders/video_call/lib/screen/cam_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:video_call/const/agora.dart';
 
 class CamScreen extends StatefulWidget{
   const CamScreen({Key? key}) : super(key: key);
@@ -9,6 +11,10 @@ class CamScreen extends StatefulWidget{
 }
 
 class _CamScreenState extends State<CamScreen>{
+  RtcEngine? engine;
+  int? uid;
+  int? otherUid;
+
   Future<bool> init() async {
     final resp = await [Permission.camera, Permission.microphone].request();
 
@@ -17,6 +23,58 @@ class _CamScreenState extends State<CamScreen>{
 
     if(cameraPermission != PermissionStatus.granted || micPermission != PermissionStatus.granted){
       throw '카메라 또는 마이크 권한이 없습니다.';
+    }
+
+    if(engine==null){
+      engine = createAgoraRtcEngine();
+
+      await engine!.initialize(
+        RtcEngineContext(
+          appId: APP_ID,
+          channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+        ),
+      );
+
+      engine!.registerEventHandler(
+        RtcEngineEventHandler(
+          onJoinChannelSuccess: (RtcConnection connection, int elapsed){
+            print('채널에 입장했습니다. uid : ${connection.localUid}');
+            setState((){
+              this.uid = connection.localUid;
+            });
+          },
+          onLeaveChannel: (RtcConnection connection, RtcStats stats){
+            print('채널 퇴장');
+            setState((){
+              uid = null;
+            });
+          },
+          onUserJoined: (RtcConnection connection, int remoteUid, int elapsed){
+            print('상대가 채널에 입장했습니다. uid : $remoteUid');
+            setState(() {
+              otherUid = remoteUid;
+
+            });
+          },
+          onUserOffline: (RtcConnection connection, int remoteUid, UserOfflineReasonType reason){
+            print('상대가 채널에서 나갔습니다. uid : $uid');
+            setState(() {
+              otherUid = null;
+            });
+          },
+        ),
+      );
+
+      await engine!.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
+      await engine!.enableVideo();
+      await engine!.startPreview();
+      await engine!.joinChannel(
+    token: TEMP_TOKEN,
+    channelId: CHANNEL_NAME,
+    options: ChannelMediaOptions(),
+    uid: 0,
+    );
+
     }
 
     return true;
@@ -43,11 +101,73 @@ class _CamScreenState extends State<CamScreen>{
               child: CircularProgressIndicator(),
             );
           }
-          return Center(
-            child: Text('모든 권한이 있습니다!'),
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:[
+              Expanded(
+                child: Stack(
+                  children:[
+                    renderMainView(),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        color: Colors.grey,
+                        height:160,
+                        width:120,
+                        child: renderSubView(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: ElevatedButton(
+                  onPressed: () async{
+                    if(engine!=null){
+                      await engine!.leaveChannel();
+                    }
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('채널 나가기'),
+                ),
+              ),
+            ],
           );
+
         }
       ),
     );
+  }
+
+  Widget renderSubView(){
+    if(uid != null){
+      return AgoraVideoView(
+        controller: VideoViewController(
+          rtcEngine: engine!,
+          canvas: const VideoCanvas(uid: 0),
+        ),
+      );
+    }else{
+      return CircularProgressIndicator();
+    }
+  }
+  Widget renderMainView(){
+    if(otherUid != null){
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          rtcEngine: engine!,
+          canvas: VideoCanvas(uid: otherUid),
+          connection: const RtcConnection(channelId: CHANNEL_NAME),
+        ),
+      );
+    }else{
+      return Center(
+        child:const Text(
+          '다른 사용자가 입장할 떄까지 대기해주세요.',
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
   }
 }
